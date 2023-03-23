27ed9ea849f3332d5efb058f5eb858def2170683
Displesher <Displesher@gmail.com>
2023-03-23 16:19:35 +0300

    modified main cpp file

--- ConsoleCpp/main.cpp
+++ ConsoleCpp/main.cpp
@@ -6,65 +6,12 @@ using std::string;
 using std::cout;
 using std::endl;
 
-enum variant
-{
-	rock,
-	scissors,
-	paper
-};
-
-class Player
-{
-private:
-	string name;
-	variant choice;
-public:
-	Player()
-	{
-		this->name = "Bot";
-		this->choice = variant::rock;
-	}
-	Player(string name, variant choice)
-	{
-		this->name = name;
-		this->choice = choice;
-	}
-	string who_wins(Player player)
-	{
-		string who_wins;
-
-		if (
-			 (this->choice == paper && player.choice == rock) ||
-			 (this->choice == rock && player.choice == scissors) ||
-			 (this->choice == scissors && player.choice == paper)
-			)
-		{
-			who_wins = "Победил игрок с именем: " + this->name;
-		}
-		if (
-			(player.choice == paper && this->choice == rock) ||
-			(player.choice == rock && this->choice == scissors) ||
-			(player.choice == scissors && this->choice == paper)
-			)
-		{
-			who_wins = "Победил игрок с именем: " + player.name;
-		}
-		else if (this->choice == player.choice)
-		{
-			who_wins = "Ничья";
-		}
-
-		return who_wins;
-	}
-};
-auto main() -> int
+int main()
 {
 	setlocale(LC_ALL, "RU");
 
-	Player bot;
-	Player alex("Alex", variant::paper);
-
-	cout << bot.who_wins(alex) << endl;
+	int res = 10;
+	std::cout << res;
 
 	return 0;
 }
